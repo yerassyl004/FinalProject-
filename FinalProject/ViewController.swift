4fbd8e8467b1f941249c061fc7e59132e5181fa2
yerassyl004 <erasil.er94@gmail.com>
2023-12-16 10:24:12 +0600

    Initial Commit

---
+++ FinalProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by Ерасыл Еркин on 16.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
